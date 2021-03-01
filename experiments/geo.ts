06972ff0613b5b6ca96947385c4c28787080fce1
Jorik Tangelder <jorik@google.com>
2021-03-01 08:38:15 +0100

    json-ld utils + geo example

---
+++ experiments/geo.ts
@@ -0,0 +1,124 @@
+/**
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {GeoCoordinates, Place} from 'schema-dts';
+import {TransmatTransfer} from '../src';
+import * as jsonLd from '../src/json_ld';
+
+const container = document.querySelector<HTMLElement>('.map');
+
+// Simple LeafLet instance.
+const L = window.L;
+const map = L.map(container, {
+  center: [47.3769, 8.5417],
+  zoom: 14,
+});
+
+const geoJsonLayer = L.layerGroup([]).addTo(map);
+
+// Setup Leaflet tiles using OpenStreetMap to avoid dealing with API keys.
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
+
+const GEO_JSON_MIME_TYPE = 'application/geo+json';
+
+// Receive at the map.
+TransmatTransfer.addReceiveListeners(container, event => {
+  const transfer = new TransmatTransfer(event);
+  const hasJsonLd = transfer.hasType(jsonLd.MIME_TYPE);
+  const hasGeoJson = transfer.hasType(GEO_JSON_MIME_TYPE);
+
+  if ((hasJsonLd || hasGeoJson) && transfer.acceptTransfer()) {
+    if (hasGeoJson) {
+      // Simply arse the receiving Geo JSON data and send to Leaflet.
+      const geoJson = JSON.parse(transfer.getData(GEO_JSON_MIME_TYPE));
+      addGeoJson(geoJson);
+      return;
+    }
+
+    if (hasJsonLd) {
+      const data = jsonLd.parse(transfer.getData(jsonLd.MIME_TYPE));
+      const features = jsonLd
+        // Find all GeoCoordinates in the JSON-LD payload.
+        .findAllOfType<GeoCoordinates>(data, 'GeoCoordinates')
+        .filter(gc => gc.latitude && gc.longitude)
+        // Map the GeoCoordinates to a GeoJson feature.
+        .map(gc => {
+          return {
+            type: 'Feature',
+            properties: {
+              name: jsonLd.getValue(gc.name),
+              popupContent: jsonLd.getValue(gc.name || gc.description),
+            },
+            geometry: {
+              type: 'Point',
+              coordinates: [gc.longitude, gc.latitude],
+            },
+          };
+        });
+      addGeoJson((features as unknown) as GeoJSON.GeoJSON);
+    }
+  }
+});
+
+function addGeoJson(data: GeoJSON.GeoJSON) {
+  // For debugging purposes.
+  console.log('displaying GeoJSON', data);
+
+  geoJsonLayer.clearLayers();
+  const feature = L.geoJSON(data).addTo(geoJsonLayer);
+  map.fitBounds(feature.getBounds());
+}
+
+// Example transmit sources.
+
+TransmatTransfer.addTransmitListeners(
+  document.querySelector('.sources .json-ld'),
+  event => {
+    const transfer = new TransmatTransfer(event);
+    const data = jsonLd.fromObject<Place>({
+      '@type': 'Place',
+      geo: {
+        '@type': 'GeoCoordinates',
+        latitude: 40.74836,
+        longitude: -73.98565,
+      },
+      name: 'Empire State Building',
+    });
+    transfer.setData(jsonLd.MIME_TYPE, data);
+  }
+);
+
+TransmatTransfer.addTransmitListeners(
+  document.querySelector('.sources .geo-json'),
+  event => {
+    const transfer = new TransmatTransfer(event);
+    const geoJsonString = JSON.stringify({
+      type: 'Feature',
+      properties: {
+        name: 'GelreDome',
+        popupContent: 'This is where the Vitesse plays!',
+      },
+      geometry: {
+        type: 'Point',
+        coordinates: [5.89287, 51.96275],
+      },
+    });
+    transfer.setData(GEO_JSON_MIME_TYPE, geoJsonString);
+  }
+);
--- src/json_ld.ts
+++ src/json_ld.ts
@@ -14,7 +14,13 @@
  * limitations under the License.
  */
 
-import {Thing, WithContext} from 'schema-dts';
+import {Thing, WithContext, DataType, PronounceableText} from 'schema-dts';
+
+export type SchemaValue<T> = T | ReadonlyArray<T>;
+export type ScalarDataType = string | number | boolean | null;
+
+// Same as WithContext, adds a @type key typing to the object.
+export declare type WithType<T> = T & {'@type': SchemaValue<string>};
 
 /** Mime type for JSON-LD data. */
 export const MIME_TYPE = 'application/ld+json';
@@ -23,7 +29,20 @@ export const MIME_TYPE = 'application/ld+json';
 export function parse<T extends Thing>(
   data: string
 ): WithContext<T> | undefined {
-  return JSON.parse(data) as WithContext<T>;
+  return JSON.parse(data, (key, value) => {
+    switch (value) {
+      case 'True':
+      case 'https://schema.org/True':
+        return true;
+
+      case 'False':
+      case 'https://schema.org/False':
+        return false;
+
+      default:
+        return value;
+    }
+  }) as WithContext<T>;
 }
 
 /**
@@ -35,10 +54,97 @@ export function fromObject<T extends Thing>(
 ): WithContext<T> {
   return Object.assign(
     {
-      // By default, map data against a https://schema.org/Thing.
       '@context': 'https://schema.org',
       '@type': 'Thing',
     },
     data
   ) as WithContext<T>;
+}
+
+/**
+ * Get the scalar value from a JSON-LD object property. In case the input is an
+ * array, the first item will be used.
+ */
+export function getValue(
+  input: SchemaValue<DataType> | undefined
+): ScalarDataType | undefined {
+  const value = Array.isArray(input) ? input[0] : input;
+  const pronounceableText = findOneOfType<Exclude<PronounceableText, string>>(
+    value,
+    'PronounceableText'
+  );
+  if (pronounceableText && pronounceableText.textValue) {
+    return getValue(pronounceableText.textValue);
+  }
+  return value;
+}
+
+/** Get scalar values as an array. */
+export function getValues(
+  input: SchemaValue<DataType>
+): ReadonlyArray<ScalarDataType> {
+  const inputArray = Array.isArray(input) ? input : [input];
+  const results = [];
+  for (const v of inputArray) {
+    const scalar = getValue(v);
+    if (scalar !== undefined) {
+      results.push(scalar);
+    }
+  }
+  return results;
+}
+
+/** Traverse an object multiple values. */
+export function findAll<T>(
+  data: {},
+  matcher: (node: T | unknown) => boolean,
+  limit = 0,
+  results: T[] = []
+): ReadonlyArray<T> {
+  if (!data || (limit && results.length === limit)) {
+    return results;
+  }
+  if (matcher(data)) {
+    results.push(data as T);
+  }
+  for (const value of Object.values(data)) {
+    if (isObject(value)) {
+      findAll(value, matcher, limit, results);
+    }
+  }
+  return results;
+}
+
+/** Find a single value. */
+export function findOne<T>(
+  data: {},
+  matcher: (node: T | unknown) => boolean
+): T | undefined {
+  return findAll<T>(data, matcher, 1)[0];
+}
+
+export function findAllOfType<T>(
+  data: {},
+  type: string,
+  limit = 0
+): ReadonlyArray<T> {
+  return findAll<T>(data, obj => isType(obj, type), limit);
+}
+
+export function findOneOfType<T>(data: {}, type: string): T | undefined {
+  return findAllOfType<T>(data, type, 1)[0];
+}
+
+/** Whether the input object is of the given type. */
+export function isType<T>(obj: T | unknown, type: string): obj is WithType<T> {
+  if (!isObject(obj)) {
+    return false;
+  }
+  const objType = (obj as WithType<T>)['@type'];
+  return Array.isArray(objType) ? objType.includes(type) : objType === type;
+}
+
+/** Whether the input is a non-null object.  */
+function isObject(input: {} | unknown): input is {} {
+  return input !== null && typeof input === 'object';
 }
